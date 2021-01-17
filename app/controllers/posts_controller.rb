8c4b3c128f40460223b5598b1f1fa65385a22c7a
kinuno <kinu.hirata@gmail.com>
2021-01-17 10:19:19 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  def index 
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+  
+end
+
+# Post.create(content: params[:content])
+# の左右のcontentは一致させないと空欄で保存されてしまった（net.html.erbファイルも一緒に変更していても）
+# なのでどちらもきちんと保存するカラム名を記載しよう。
